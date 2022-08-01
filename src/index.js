91157bc2c3f44af0b196971fe877d960c8a93a05
Lukas Buck <lukas.buck@e-mail.de>
2022-08-01 14:14:24 +0200

    routers

--- src/index.js
+++ src/index.js
@@ -2,94 +2,15 @@ const express = require('express')
 require('./db/mongoose')
 const User = require('./models/user')
 const Task = require('./models/task')
+const userRouters = require("./routes/user")
+const taskRouters = require("./routes/task")
 
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.json())
-
-app.post('/users', (req, res) => {
-    const user = new User(req.body)
-
-    user.save().then(() => {
-        res.status(201).send(user)
-    }).catch((e) => {
-        res.status(400).send(e)
-    })
-})
-
-app.get("/users", (req, res) => {
-    User.find({}).then((users) => {
-        res.send(users)
-    }).catch((e) => {
-        res.status(500).send()
-    })
-})
-
-app.get("/users/:id", (req, res) => {
-    const _id = req.params.id
-
-    User.findById(_id).then((user) => {
-        if(!user){
-            return res.status(404).send()
-        }
-        res.send(user)
-    }).catch((e) => {
-        res.status(500).send()
-    })
-})
-
-app.patch("/users/:id", async(req, res) => {
-    const updates = Object.keys(req.body)
-    const allowedUpdates = ['name', 'email', 'age', 'password']
-    const isValidOperation = updates.every((update) =>  allowedUpdates.includes(update))
-
-    if(!isValidOperation){ 
-        return res.status(400).send({error: 'Invalid updates!'})
-    }
-    try{
-        const user = await User.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true})
-
-        if(!user){
-            return res.status(404).send()
-        }
-
-        res.send(user)
-    }catch(e){
-        res.status(400).send(e)
-    }
-})
-
-app.get("/tasks/:id", (req, res) => {
-    const _id = req.params.id
-
-    Task.findById(_id).then((tasks) => {
-        if(!tasks){
-            return res.status(404).send()
-        }
-        res.send(tasks)
-    }).catch((e) => {
-        res.status(500).send()
-    })
-})
-
-app.get("/tasks", (req, res) => {
-    Task.find({}).then((tasks) => {
-        res.send(tasks)
-    }).catch((e) => {
-        res.status(500).send()
-    })
-})
-
-app.post("/tasks", (req, res) => {
-    const task = new Task(req.body)
-
-    task.save().then(() => {
-        res.status(201).send(task)
-    }).catch((e) => {
-        res.status(400).send(e)
-    })
-})
+app.use(userRouters)
+app.use(taskRouters)
 
 
 
---
+++ src/routes/task.js
@@ -0,0 +1,52 @@
+const express = require("express")
+const router = new express.Router()
+const Task = require('../models/task')
+
+router.get("/tasks/:id", (req, res) => {
+    const _id = req.params.id
+
+    Task.findById(_id).then((tasks) => {
+        if(!tasks){
+            return res.status(404).send()
+        }
+        res.send(tasks)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+
+router.get("/tasks", (req, res) => {
+    Task.find({}).then((tasks) => {
+        res.send(tasks)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+
+router.post("/tasks", (req, res) => {
+    const task = new Task(req.body)
+
+    task.save().then(() => {
+        res.status(201).send(task)
+    }).catch((e) => {
+        res.status(400).send(e)
+    })
+})
+
+router.delete("/tasks/:id", async(req,res) =>{
+    try{
+       const task = await Task.findByIdAndDelete(req.params.id)
+       if(!task){
+        res.status(400).send()
+       }
+
+       res.send(task)
+    }catch(e){
+        res.status(500).send()
+
+    }
+
+
+})
+
+module.exports = router
