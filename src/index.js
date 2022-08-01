83eedb96ad2dd48e49e6c2a30e99cc1a8d7a6d80
Lukas Buck <lukas.buck@e-mail.de>
2022-08-01 15:23:43 +0200

    Updates

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 const express = require('express')
 require('./db/mongoose')
-const User = require('./models/user')
-const Task = require('./models/task')
 const userRouters = require("./routes/user")
-const taskRouters = require("./routes/task")
 
 
 const app = express()
@@ -11,7 +8,6 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 app.use(userRouters)
-app.use(taskRouters)
 
 
 
