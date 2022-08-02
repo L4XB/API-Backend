5fd7b6bc7a9cb0616d6203c80fe4e47edf988fca
Lukas Buck <lukas.buck@e-mail.de>
2022-08-02 15:12:58 +0200

    init

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -4,8 +4,9 @@ mongoose.connect(
     `mongodb+srv://Lukas3009:hxNxo86FSmcUL2el@cluster0.nsxbz.mongodb.net/chaining`, 
     {
       useNewUrlParser: true,
-      useFindAndModify: false,
-      useUnifiedTopology: true
+      useUnifiedTopology: true,
+      useCreateIndex: true
+
     }
   );
 
